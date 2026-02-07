dcbf1db3154d6f735a9e4c1c3051f25785f3f907
Rayan-Siddiqui <rmsiddiqui115@gmail.com>
2026-02-07 14:08:24 -0500

    Added Main.js and linked it to Index

---
+++ css/js/main.js
@@ -0,0 +1,10 @@
+/*
+Name: Rayan Siddiqui
+Student ID: 953391424
+Course: IST 256
+Assignment: DOM Portfolio Enhancement
+Submission Date: 02/07/2025
+Portfolio URL: https://rayan-siddiqui.github.io/Rayan-Siddiqui-Portfolio/
+*/
+
+
